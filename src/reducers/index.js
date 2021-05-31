3a5bb944f89a1ec49e32ac876e763d61d5ca8e15
imran5644 <imi56444@gmail.com>
2021-05-31 22:41:49 +0800

    Creates books actions

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'react-redux';
+import booksReducer from './books';
+
+const rootReducer = combineReducers({
+  books: booksReducer,
+});
+
+export default rootReducer;
