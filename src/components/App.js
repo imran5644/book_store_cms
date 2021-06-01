e63647308a6a89eb719a60dfc2ca12f597768d79
imran5644 <imi56444@gmail.com>
2021-06-01 21:42:25 +0800

    Creates methods to add new books

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import BookList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import { createBook, deleteBook } from '../actions';
 
-const App = () => (
+const App = ({ books, create: handleCreate, delete: handleDelete }) => (
   <div className="App">
-    <BookList />
-    <BooksForm />
+    <BookList books={books} />
+    <BooksForm create={handleCreate} delete={handleDelete} />
   </div>
 );
 
-export default App;
+App.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  create: PropTypes.func.isRequired,
+  delete: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({ books: state.books });
+const mapDispatchToProps = (dispatch) => ({
+  create: (book) => dispatch(createBook(book)),
+  delete: (book) => dispatch(deleteBook(book)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
