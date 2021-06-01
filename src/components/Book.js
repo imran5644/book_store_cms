43db5580731e577aeddf054d8ee8883b162997ff
imran5644 <imi56444@gmail.com>
2021-06-01 21:51:14 +0800

    Adds button to delete books

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => (
+const Book = ({ book, delete: handleDelete }) => (
   <table className="table">
     <tr>
       <td>{book.id}</td>
       <td>{book.title}</td>
       <td>{book.category}</td>
+      <td>
+      <button type="button" onClick={() => handleDelete(book)}>
+        <i className="fa fa-trash" />
+      </button>
+    </td>
     </tr>
   </table>
 );
@@ -17,6 +22,7 @@ Book.propTypes = {
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   }).isRequired,
+  delete: PropTypes.func.isRequired,
 };
 
 export default Book;
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux';
 import Book from '../components/Book';
 import { createBook, deleteBook } from '../actions';
 
-const BookList = ({ books }) => (
+const BookList = ({ books, delete: handleDelete }) => (
   books.map(
-    (book) => <tr key={book.id}><Book book={book} /></tr>,
+    book => <tr key={book.id}><Book book={book} delete={handleDelete} /></tr>,
   )
 );
 
@@ -18,6 +18,7 @@ BookList.propTypes = {
       category: PropTypes.string.isRequired,
     }),
   ).isRequired,
+  delete: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({ books: state.books });
