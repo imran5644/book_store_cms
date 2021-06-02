b56d5fa2e26ad5cf1aadaaaba4d5f3b9af49d2cd
imran5644 <imi56444@gmail.com>
2021-06-02 22:51:35 +0800

    Adds tbody to table and fix typo on placeholder

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 ReactDOM.render(
   <Provider store={store}>
     <App />
